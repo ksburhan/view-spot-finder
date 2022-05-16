ff90cc17a073663b2f51ec34db1b6bf7811a8e68
ksburhan <burhan.koeseler@hotmail.de>
2022-05-16 15:16:02 +0200

    Computes local maxima, but very slow

--- view_spot_finder.py
+++ view_spot_finder.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import time
 
 
 def sort_by_key(values):
@@ -7,16 +8,19 @@ def sort_by_key(values):
 
 
 if __name__ == '__main__':
+    start = time.time()
     arguments = sys.argv
 
     mesh_file = arguments[1]
-    number_view_spots = arguments[2]
+    number_view_spots = int(arguments[2])
+    view_spots = []
     view_spots_found = 0
 
     f = open(mesh_file)
     mesh = json.load(f)
     f.close()
     sorted_values = sorted(mesh["values"], key=sort_by_key, reverse=True)
+    elements_with_neighbours = []
     for value in sorted_values:
         element_id = value["element_id"]
         nodes = mesh["elements"][element_id]["nodes"]
@@ -26,4 +30,27 @@ if __name__ == '__main__':
                 continue
             else:
                 neighbours.append(element["id"])
+        elements_with_neighbours.append((element_id, neighbours))
 
+    mid = time.time()
+    print(mid - start)
+
+    for element in elements_with_neighbours:
+        if view_spots_found == number_view_spots:
+            break
+        is_maxima = True
+        own_height = mesh["values"][element[0]]["value"]
+        for neighbour in element[1]:
+            neighbour_height = mesh["values"][neighbour]["value"]
+            if own_height < neighbour_height:
+                is_maxima = False
+                break
+        if not is_maxima:
+            continue
+        else:
+            view_spots.append(mesh["values"][element[0]])
+            view_spots_found = view_spots_found + 1
+
+    print(view_spots)
+    end = time.time()
+    print(end-start)

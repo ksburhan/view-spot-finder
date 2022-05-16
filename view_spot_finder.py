10825986c5a66e1c3116798cf15aeb4daafd8e53
ksburhan <burhan.koeseler@hotmail.de>
2022-05-16 17:13:16 +0200

    Adds neighbouring elements that have been visited and known not to be a maximum to a list and skips checking it

--- view_spot_finder.py
+++ view_spot_finder.py
@@ -8,44 +8,59 @@ def sort_by_key(values):
 
 
 if __name__ == '__main__':
+    # set variables and load data
     start = time.time()
     arguments = sys.argv
-
     mesh_file = arguments[1]
     number_view_spots = int(arguments[2])
     view_spots = []
     view_spots_found = 0
-
     f = open(mesh_file)
     mesh = json.load(f)
     f.close()
+    visited_nodes = []
+
+    # sort height values in descending order
     sorted_values = sorted(mesh["values"], key=sort_by_key, reverse=True)
-    elements_with_neighbours = []
-    for value in sorted_values:
-        if view_spots_found == number_view_spots:
-            break
-        element_id = value["element_id"]
-        nodes = mesh["elements"][element_id]["nodes"]
+
+    # for each height value of target elements check if not already visited (known as not maximum)
+    for target_value in sorted_values:
+        target_element_id = target_value["element_id"]
+        target_nodes = mesh["elements"][target_element_id]["nodes"]
         neighbours = []
+        if target_element_id in visited_nodes:
+            continue
+
+        # find all neighbours to target element (atleast one vertex shared)
         for element in mesh["elements"]:
-            if element["id"] is element_id or not any(n in element["nodes"] for n in nodes):
+            if element["id"] is target_element_id or \
+                    not any(n in element["nodes"] for n in target_nodes):
                 continue
             else:
                 neighbours.append(element["id"])
+
+        # check if any of the neighbours have higher value
+        # if yes -> current element is not maximum
+        # if no -> current element is maximum
+        # every neighbour that has smaller height value cannot be maximum -> is added to the visited nodes
         is_maxima = True
-        own_height = value["value"]
+        own_height = target_value["value"]
         for neighbour in neighbours:
             neighbour_height = mesh["values"][neighbour]["value"]
             if own_height < neighbour_height:
                 is_maxima = False
                 break
-        if not is_maxima:
-            continue
-        else:
-            view_spots.append(mesh["values"][element_id])
-            view_spots_found = view_spots_found + 1
-        elements_with_neighbours.append((element_id, neighbours))
+            else:
+                visited_nodes.append(neighbour)
 
-    print(view_spots)
-    end = time.time()
-    print(end - start)
+        # if all neighbours have smaller height value, current element is a maximum and added to the list
+        if is_maxima:
+            view_spots.append(mesh["values"][target_element_id])
+            view_spots_found = view_spots_found + 1
+        else:
+            visited_nodes.append(target_element_id)
+
+        if view_spots_found == number_view_spots:
+            break
+
+    print(json.dumps(view_spots, indent=2, separators=(',', ': ')))
